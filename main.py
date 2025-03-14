7c1820eb6f8c08ec210604cd5ba60644ff9e1428
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-14 03:43:13 -0400

    updated main with some print statements

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughtout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {1280}")
+    print(f"Screen height: {720}")
+
+if __name__ == "__main__":
+    main()
+
