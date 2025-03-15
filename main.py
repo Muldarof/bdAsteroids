861c6a9d1380643917639bc1ffa2ee9b68d15bd6
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-15 01:06:12 -0400

    updated main loop with a time clock and fps limiter

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {1280}")
     print(f"Screen height: {720}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (1,1,1))
+        screen.fill("black")
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time / 1000
 
 
 if __name__ == "__main__":
