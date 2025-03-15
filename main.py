b9f6382c3d8853aa47e59f09527191ec2970f67b
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-15 02:54:43 -0400

    instantiated new player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughtout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -14,6 +15,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    mainCharacter = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
