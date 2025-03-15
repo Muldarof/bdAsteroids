370901b6fe0511789dda41650e97d088f4c27eb0
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-15 00:39:45 -0400

    updated main with display surface

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {1280}")
     print(f"Screen height: {720}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    counter = 0
+    while True:
+        pygame.Surface.fill(screen, (1,1,1))
+        pygame.display.flip()
+        print(f"Counter = {counter}")
+        counter += 1
 
 if __name__ == "__main__":
     main()
