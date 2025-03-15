ad315a99c0017bd74fba6edaa3cb7d1ef25e35ad
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-03-15 03:06:10 -0400

    added comments for easy readability

--- main.py
+++ main.py
@@ -11,19 +11,30 @@ def main():
     print(f"Screen height: {720}")
 
     pygame.init()
+    #create screen with set dimensions
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #create an instance to keep track of time, used for limiting fps
     clock = pygame.time.Clock()
     dt = 0
     
+    #create entities
     mainCharacter = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, 10)
 
+    #main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Draw background (black)
         screen.fill("black")
+        
+        #Draw everything
         mainCharacter.draw(screen)
+
+        #Update drawing to screen
         pygame.display.flip()
+
+        #limit screen updates to 60 fps
         time = clock.tick(60)
         dt = time / 1000
         
